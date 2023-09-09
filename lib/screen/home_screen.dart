b57940af5cf0194984cbd0b52f6af9e7a3db84ac
mcalisadeghi <ali.ke970@gmail.com>
2023-09-09 21:49:47 +0330

    add slider in home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -161,8 +161,8 @@ class HomeScreen extends StatelessWidget {
                         margin: const EdgeInsets.only(
                           right: 15,
                         ),
-                        color: Colors.red,
-                        height: 200,
+                        // color: Colors.red,
+                        height: 280,
                         child: ListView.builder(
                           itemCount: imageList.length,
                           scrollDirection: Axis.horizontal,
@@ -177,7 +177,8 @@ class HomeScreen extends StatelessWidget {
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   SizedBox(
-                                    height: 200,
+                                    height: 180,
+                                    width: 150,
                                     child: Stack(
                                       children: [
                                         InkWell(
@@ -188,6 +189,9 @@ class HomeScreen extends StatelessWidget {
                                             ),
                                             child: Image.asset(
                                               imageList[index],
+                                              fit: BoxFit.cover,
+                                              height: 180,
+                                              width: 150,
                                             ),
                                           ),
                                         ),
@@ -226,7 +230,25 @@ class HomeScreen extends StatelessWidget {
                                       fontSize: 18,
                                       fontWeight: FontWeight.bold,
                                     ),
-                                  )
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.star,
+                                        size: 25,
+                                      ),
+                                      Text(
+                                        '(${prices[index]})',
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ],
                               ),
                             );
@@ -235,6 +257,113 @@ class HomeScreen extends StatelessWidget {
                       );
                     },
                   ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Newest product',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                GridView.builder(
+                  itemCount: productTitles.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.6,
+                    crossAxisSpacing: 2,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: const EdgeInsets.only(
+                        right: 15,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 200,
+                            child: Stack(
+                              children: [
+                                InkWell(
+                                  onTap: () {},
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(
+                                      10,
+                                    ),
+                                    child: Image.asset(
+                                      imageList[index],
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  right: 10,
+                                  top: 10,
+                                  child: Container(
+                                    height: 30,
+                                    width: 30,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(
+                                        20,
+                                      ),
+                                    ),
+                                    child: const Center(
+                                      child: Icon(
+                                        Icons.favorite,
+                                        color: Color(
+                                          0xFFDB3022,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            productTitles[index],
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.star,
+                                size: 25,
+                              ),
+                              Text(
+                                '(${prices[index]})',
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
