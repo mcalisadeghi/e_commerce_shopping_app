8d19325f195f7b4710423d84639bb8a256858405
mcalisadeghi <ali.ke970@gmail.com>
2023-09-13 20:15:40 +0330

    build ContainerButtonModel

---
+++ lib/widgets/container_button_modal.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ContainerButtonModel extends StatelessWidget {
+  final Color? bgColor;
+  final double containerwidth;
+  final String itext;
+
+  const ContainerButtonModel({
+    super.key,
+    this.bgColor,
+    required this.containerwidth,
+    required this.itext,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
